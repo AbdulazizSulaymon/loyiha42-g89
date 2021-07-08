ca8fb0bb2da8ed5050d9a6e48b91c085edd4f763
AbdulazizSulaymon <djshahrick@gmail.com>
2021-07-08 18:26:50 +0500

    ui changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./logo.svg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React bo'yicha 1 - loyiha G89 guruh bilan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="shadow">
+        <div className="container py-3 d-flex align-items-center">
+          <img src={logo} className="logo" alt="" />
+          <p className="fw-bold mb-0 me-5">React App 1</p>
+
+          <ul className="d-flex">
+            <li className="d-block"><a href="#" className="text-secondary p-3">Home</a></li>
+            <li className="d-block"><a href="#" className="text-secondary p-3">Service</a></li>
+            <li className="d-block"><a href="#" className="text-secondary p-3">About</a></li>
+          </ul>
+        </div>
       </header>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-md-6">
+            <p className="text-primary fw-bold">Roberto Carlos</p>
+            <h1>Visual Designer
+              & Developer</h1>
+            <p className="text-secondary">Hello I'm Roberto Carlos. I design interfaces - the metaphors that humanize them, and the systems that scale them.
+              Let's create something together.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
